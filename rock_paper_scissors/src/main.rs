93e2d0e281c1a4cf9a65a64ad75f509639cdddb5
48exa <douwewesterdijk@gmail.com>
2023-02-14 21:13:46 +0100

    rock paper scissors from tutorial

--- rock_paper_scissors/src/main.rs
+++ rock_paper_scissors/src/main.rs
@@ -1,3 +1,123 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fmt, io, str};
+
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
+
+#[derive(Debug)]
+enum MovesError {
+    Unknown(String),
+}
+
+pub trait Compare<T, U> {
+    fn compare(&self, b: &T) -> U;
+}
+
+impl fmt::Display for MovesResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MovesResult::Win(result) => match result {
+                CompMoves::RockOverScissors => write!(f, "Rock beats scissors"),
+                CompMoves::PaperOverRock => write!(f, "Paper beats rock"),
+                CompMoves::ScissorsOverPaper => write!(f, "Scissors beats paper"),
+            },
+            MovesResult::Lose(result) => match result {
+                CompMoves::RockOverScissors => write!(f, "Rock beats scissors"),
+                CompMoves::PaperOverRock => write!(f, "Paper beats rock"),
+                CompMoves::ScissorsOverPaper => write!(f, "Scissors beats paper"),
+            },
+            MovesResult::Tie => write!(f, "You tied!"),
+        }
+    }
+}
+
+impl Compare<Moves, MovesResult> for Moves {
+    fn compare(&self, b: &Moves) -> MovesResult {
+        match self {
+            Moves::Rock => match b {
+                Moves::Rock => MovesResult::Tie,
+                Moves::Paper => MovesResult::Lose(CompMoves::PaperOverRock),
+                Moves::Scissors => MovesResult::Win(CompMoves::RockOverScissors),
+            },
+            Moves::Paper => match b {
+                Moves::Rock => MovesResult::Win(CompMoves::PaperOverRock),
+                Moves::Paper => MovesResult::Tie,
+                Moves::Scissors => MovesResult::Lose(CompMoves::ScissorsOverPaper),
+            },
+            Moves::Scissors => match b {
+                Moves::Rock => MovesResult::Lose(CompMoves::RockOverScissors),
+                Moves::Paper => MovesResult::Win(CompMoves::ScissorsOverPaper),
+                Moves::Scissors => MovesResult::Tie,
+            },
+        }
+    }
+}
+
+impl str::FromStr for Moves {
+    type Err = MovesError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "r" | "rock" => Ok(Moves::Rock),
+            "p" | "paper" => Ok(Moves::Paper),
+            "s" | "scissors" => Ok(Moves::Scissors),
+            _ => Err(MovesError::Unknown(s.to_string())),
+        }
+    }
+}
+
+impl fmt::Display for Moves {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Moves::Rock => write!(f, "Rock"),
+            Moves::Paper => write!(f, "Paper"),
+            Moves::Scissors => write!(f, "Scissors"),
+        }
+    }
+}
+
+impl Distribution<Moves> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Moves {
+        let index: u8 = rng.gen_range(0..3);
+        match index {
+            0 => Moves::Rock,
+            1 => Moves::Paper,
+            2 => Moves::Scissors,
+            _ => unreachable!(),
+        }
+    }
+}
+
+enum Moves {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum CompMoves {
+    RockOverScissors,
+    PaperOverRock,
+    ScissorsOverPaper,
+}
+
+enum MovesResult {
+    Win(CompMoves),
+    Lose(CompMoves),
+    Tie,
+}
+
+pub fn main() {
+    loop {
+        println!("Choose! (r)ock, (p)aper or (s)cissors!");
+
+        let mut um: String = String::new();
+        let cm: Moves = rand::thread_rng().gen();
+
+        io::stdin().read_line(&mut um).expect("Failed to read line");
+
+        let um: Moves = um.trim().parse().expect("This is not a valid guess");
+        let result: MovesResult = um.compare(&cm);
+
+        println!("You guessed {}", um);
+        println!("I gussed {}", cm);
+        println!("{}", result);
+    }
 }
---
+++ rock_paper_scissors/target/release/deps/ppv_lite86-9a632e951cb48449.d
@@ -0,0 +1,11 @@
+C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\deps\ppv_lite86-9a632e951cb48449.rmeta: C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\lib.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\soft.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\types.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\deps\libppv_lite86-9a632e951cb48449.rlib: C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\lib.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\soft.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\types.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\deps\ppv_lite86-9a632e951cb48449.d: C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\lib.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\soft.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\types.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\lib.rs:
+C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\soft.rs:
+C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\types.rs:
+C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\mod.rs:
+C:\Users\exa\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.17\src\x86_64\sse2.rs:
