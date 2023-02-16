7e21a58ef4a4771ee07a6025e00ec326e0a87f9f
48exa <douwewesterdijk@gmail.com>
2023-02-16 09:26:52 +0100

    gitignore stuff

--- rock_paper_scissors/target/release/deps/rock_paper_scissors.d
+++ rock_paper_scissors/target/release/deps/rock_paper_scissors.d
@@ -1,5 +1,5 @@
-C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\deps\rock_paper_scissors.exe: src\main.rs
+C:\Users\exa\Documents\GitHub\projects\rock_paper_scissors\target\release\deps\rock_paper_scissors.exe: src\main.rs
 
-C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\deps\rock_paper_scissors.d: src\main.rs
+C:\Users\exa\Documents\GitHub\projects\rock_paper_scissors\target\release\deps\rock_paper_scissors.d: src\main.rs
 
 src\main.rs:
--- rock_paper_scissors/target/release/rock_paper_scissors.d
+++ rock_paper_scissors/target/release/rock_paper_scissors.d
@@ -1,1 +1,1 @@
-C:\Users\exa\documents\github\projects\rock_paper_scissors\target\release\rock_paper_scissors.exe: C:\Users\exa\documents\github\projects\rock_paper_scissors\src\main.rs
+C:\Users\exa\Documents\GitHub\projects\rock_paper_scissors\target\release\rock_paper_scissors.exe: C:\Users\exa\Documents\GitHub\projects\rock_paper_scissors\src\main.rs
